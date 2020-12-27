1339e672386856263a7339b72ca3d956ce1dd1e1
Doruk <dorukyogscast@gmail.com>
2020-12-27 21:37:21 +0300

    new dev map

--- game/multiplayer.bat
+++ game/multiplayer.bat
@@ -3,4 +3,4 @@
 :: Here is some that are useless  -useappid SteamVRAppID +vr_enable_fake_vr_test -nonvr
 ::==============================================================
 @echo off
-START bin\win64\"Source 2.exe" -dedicated -noassert -allow_no_lobby_connect -tickrate 64 -maxplayers 8 -maxplayers_override 8 +map untitled_1.vmap -w 1920 -h 1080
+START bin\win64\"Source 2.exe" -dedicated -noassert -allow_no_lobby_connect -tickrate 64 -maxplayers 8 -maxplayers_override 8 +map dev_test.vmap -w 1920 -h 1080
