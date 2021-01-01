3aba9391e2026fc645c90d53ed2e4794b9c65c6b
Doruk <dorukyogscast@gmail.com>
2021-01-01 18:54:17 +0300

    optimized file sizes, cleared junk

--- game/launch a map.bat
+++ game/launch a map.bat
@@ -3,4 +3,4 @@
 :: Here is some that are useless  -windowed -noborder -gl
 ::==============================================================
 @echo off
-START bin\win64\"Source 2.exe" +map dev_test.vmap -w 1920 -h 1080 
+START bin\win64\"Source 2.exe" +map "dev_test.vmap" -w 1920 -h 1080 
